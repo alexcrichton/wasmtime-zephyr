5c2d6bd73e0a0c110e4f1dc7951cfacec91e8bf6
Alex Crichton <alex@alexcrichton.com>
2024-03-27 12:24:52 -0700

    Remove custom lib and tests

--- app/src/main.c
+++ app/src/main.c
@@ -6,13 +6,16 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/sensor.h>
 #include <app_version.h>
-
 #include <zephyr/logging/log.h>
+
+/* #include <custom_lib/custom_lib.h> */
+
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main(void)
 {
 	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+        /* printk("%d\n", custom_lib_get_value(1)); */
 
 	return 0;
 }
