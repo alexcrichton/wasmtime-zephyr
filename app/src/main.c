bc3ba971d4c1fdcb3036a8417c7eebe32548b0cc
Alex Crichton <alex@alexcrichton.com>
2024-03-27 12:28:17 -0700

    Small userspace

--- app/src/main.c
+++ app/src/main.c
@@ -7,16 +7,32 @@
 #include <zephyr/drivers/sensor.h>
 #include <app_version.h>
 #include <zephyr/logging/log.h>
+#include <stdio.h>
+
+#ifndef CONFIG_USERSPACE
+#error This requires CONFIG_USERSPACE.
+#endif
 
 /* #include <custom_lib/custom_lib.h> */
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+
+#define USER_STACKSIZE	2048
+struct k_thread user_thread;
+K_THREAD_STACK_DEFINE(user_stack, USER_STACKSIZE);
+
+static void user_function(void *p1, void *p2, void *p3) {
+  printf("Hello World\n");
+}
 
 int main(void)
 {
 	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
         /* printk("%d\n", custom_lib_get_value(1)); */
 
+	k_thread_create(&user_thread, user_stack, USER_STACKSIZE,
+			user_function, NULL, NULL, NULL,
+			-1, K_USER, K_MSEC(0));
 	return 0;
 }
 
